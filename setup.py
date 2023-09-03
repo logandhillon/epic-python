cc08dd4eb1bbc243d399180e736729aa99a00ebd
Logan Dhillon <logandhi64@gmail.com>
2023-09-02 23:17:09 -0400

    Started work on like packages or something i forgor

---
+++ setup.py
@@ -0,0 +1,8 @@
+import setuptools
+
+setuptools.setup(
+    name="epic-python",
+    version="0.1.0-alpha",
+    description="EPIC (Extremely Problematic and Inconventient Code) for Python!",
+    packages=["epic-python"]
+)
